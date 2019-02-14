540f7c585daec6e935bdaca3fb0a7ca84423489d
tomasjgiron <47337943+tomasjgiron@users.noreply.github.com>
2019-02-13 22:21:33 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -4,30 +4,64 @@ A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%.
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
 C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos e informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
- 	var precio = 35;
+function CalcularPrecio() {
+    var precio;
     var cantidad;
-    var descuento;
-    var precioFinal;
-
-    
-
+    var preciofinal;
+    var marca;
+    var unidad;
 
     cantidad = parseInt(document.getElementById("Cantidad").value);
 
-    if (cantidad >= 6){
+    precio = 35;
 
-        descuento = precio * 50 / 100;
+    unidad = cantidad * precio;
 
-        precioFinal = precio - descuento;
+    document.getElementById("precioDescuento").value = unidad;
 
-        document.getElementById("precioDescuento").value = precioFinal.toFixed(2);
+    marca = document.getElementById("Marca").value;
 
-    } else if (
-    
+
+    if (cantidad >= 6) 
+    {
+
+        preciofinal = unidad / 2;
+
+        document.getElementById("precioDescuento").value = preciofinal.toFixed(2);
+
+    }
+    else if (cantidad == 5) 
+    {
+        if (marca == "ArgentinaLuz") 
+        {
+            preciofinal = unidad * 40 / 100;
+        }else 
+        {
+            preciofinal = unidad * 30 / 100; 
+        }
+    }
+    else if (cantidad == 4) 
+    {
+        if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") 
+        {
+            preciofinal = unidad * 25 / 100;
+        }else 
+        {
+            preciofinal = unidad * 20 / 100; 
+        }
+    }
+    else if (cantidad == 5) 
+    {
+        if (marca == "ArgentinaLuz") 
+        {
+            preciofinal = unidad * 40 / 100;
+        }else 
+        {
+            preciofinal = unidad * 30 / 100; 
+        }
+    }
 }
