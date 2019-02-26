49a57a325bd52e52ec76500d0f513a51282a3bfd
tomasjgiron <47337943+tomasjgiron@users.noreply.github.com>
2019-02-26 00:17:34 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,12 @@
-function Mostrar()
-{
+function Mostrar() {
 	//Genero el número RANDOM entre 1 y 10 
+	var max = 10;
+	var min = 1;
 
+	var num;
+
+	num = Math.floor(Math.random() * ((max + 1) - min) + min);
+
+	alert(num);
 
 }//FIN DE LA FUNCIÓN
