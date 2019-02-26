e2151a4438a84b6be448c8041bc980e9b1a81890
tomasjgiron <47337943+tomasjgiron@users.noreply.github.com>
2019-02-26 00:16:30 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -9,59 +9,56 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 
  */
 function CalcularPrecio() {
-    var precio;
-    var cantidad;
-    var preciofinal;
-    var marca;
-    var unidad;
 
-    cantidad = parseInt(document.getElementById("Cantidad").value);
+    var cantidad = parseInt(document.getElementById("Cantidad").value);
+    var marca = document.getElementById("Marca").value;
+    var precio = 35;
+    var descuento;
+    var precioDescuento;
+    var precioFinal;
+    var iibb;
 
-    precio = 35;
+    if (cantidad >= 6){
+        descuento = precio * 50 / 100;
 
-    unidad = cantidad * precio;
-
-    document.getElementById("precioDescuento").value = unidad;
-
-    marca = document.getElementById("Marca").value;
-
-
-    if (cantidad >= 6) 
-    {
-
-        preciofinal = unidad / 2;
-
-        document.getElementById("precioDescuento").value = preciofinal.toFixed(2);
-
-    }
-    else if (cantidad == 5) 
-    {
-        if (marca == "ArgentinaLuz") 
-        {
-            preciofinal = unidad * 40 / 100;
-        }else 
-        {
-            preciofinal = unidad * 30 / 100; 
+    } else if (cantidad == 5){
+        if (marca == "ArgentinaLuz"){
+            descuento = precio * 40 / 100;
         }
-    }
-    else if (cantidad == 4) 
-    {
-        if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") 
-        {
-            preciofinal = unidad * 25 / 100;
-        }else 
-        {
-            preciofinal = unidad * 20 / 100; 
+        else {
+            descuento = precio * 30 / 100;
         }
-    }
-    else if (cantidad == 5) 
-    {
-        if (marca == "ArgentinaLuz") 
-        {
-            preciofinal = unidad * 40 / 100;
-        }else 
-        {
-            preciofinal = unidad * 30 / 100; 
+        
+    } else if (cantidad == 4){
+        if (marca == "ArgentinaLuz" || marca == "FelipeLamparas"){
+            descuento = precio * 25 / 100;
         }
+        else {
+            descuento = precio * 20 / 100;
+        }
+    } else if (cantidad == 3){
+        if (marca == "ArgentinaLuz"){
+            descuento = precio * 30 / 100;
+        }
+        else if (marca == "FelipeLamparas"){
+            descuento = precio * 10 / 100;
+        }
+        else {
+            descuento = precio * 5 / 100;
+        }
+    } else {
+        descuento = 0;
     }
+        precioDescuento = precio - descuento;
+        precioFinal = cantidad * precioDescuento;
+        document.getElementById("precioDescuento").value = precioDescuento;
+
+        if(precioFinal > 120){
+            iibb = precioFinal * 10 / 100;
+            precioFinal = precioFinal + iibb;
+            alert ("Importe final $ " + precioFinal + " , IIBB usted pago $ " + iibb);
+        }
+        else {
+            alert ("Importe final $ " + precioFinal);
+        }
 }
